70d05b698379f191d9bbc8ad2522c3039d0b0218
PiccandN <jagermoffin@gmail.com>
2013-11-22 14:10:35 +0100

    Update 08-visite-de-la-mine-obligatoire.py

--- niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
+++ niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 08-visite-de-la-mine-obligatoire.py
 # nom de l'exercice : Visite de la mine
@@ -17,3 +16,26 @@
 
 # mettre votre code ici
 
+nbDeplacements = int(input())
+aller = [0] * nbDeplacements
+retour = [0] * nbDeplacements
+valeurRetour = 0
+
+for tDeplacement in range(nbDeplacements):
+   tDeplacement = int(input())
+   if tDeplacement == 1:
+      retour[valeurRetour] = 2
+   elif tDeplacement == 2:
+      retour[valeurRetour] = 1 
+   elif tDeplacement == 4:
+      retour[valeurRetour] = 5
+   elif tDeplacement == 5:
+      retour[valeurRetour] = 4
+   else:
+      retour[valeurRetour] = 3
+   valeurRetour += 1
+   
+valeurRetour -= 1
+for loop in range(nbDeplacements):
+   print(retour[valeurRetour])
+   valeurRetour -= 1
