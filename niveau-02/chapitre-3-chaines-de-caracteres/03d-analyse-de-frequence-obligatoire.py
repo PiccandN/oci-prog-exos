82a5fc2bf7d61f0a9c52d4d443c212bc275f7e68
PiccandN <jagermoffin@gmail.com>
2013-12-10 16:12:11 +0100

    Update 03d-analyse-de-frequence-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/03d-analyse-de-frequence-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/03d-analyse-de-frequence-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 03d-analyse-de-frequence-obligatoire.py
 # nom de l'exercice : Analyse de fréquence
@@ -17,3 +16,19 @@
 
 # mettre votre code ici
 
+nbLignes, nbMots = map(int, input().split())
+ligne = [0] * nbLignes
+mots = []
+
+for idLigne in range(nbLignes):
+   ligne[idLigne] = input()
+   mots += ligne[idLigne].split() #création d'un tableau avec tous les mots dedans
+   
+taille = [0] * 101
+
+for mot in mots:
+   taille[len(mot)] += 1
+
+for i in range(101):
+   if taille[i] >= 1:
+      print("{} : {}".format(i, taille[i]))
