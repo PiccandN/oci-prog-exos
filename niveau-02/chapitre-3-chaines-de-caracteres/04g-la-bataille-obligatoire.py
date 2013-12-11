0bb9bd389389eba3d7c8383c46fe2c99faf68bbc
PiccandN <jagermoffin@gmail.com>
2013-12-11 16:53:33 +0100

    Update 04g-la-bataille-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/04g-la-bataille-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/04g-la-bataille-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04g-la-bataille-obligatoire.py
 # nom de l'exercice : La bataille
@@ -17,3 +16,32 @@
 
 # mettre votre code ici
 
+cartes1 = input()
+cartes2 = input()
+victoire = False
+nbTours = 0
+
+if cartes1 < cartes2:
+   longueur = len(cartes1)
+else:
+   longueur = len(cartes2)
+
+while victoire == False and longueur > nbTours:
+   if cartes1[nbTours] == cartes2[nbTours]:
+      nbTours += 1
+   elif cartes1[nbTours] < cartes2[nbTours]:
+      print(1)
+      victoire = True
+   elif cartes1[nbTours] > cartes2[nbTours]:
+      print(2)
+      victoire = True
+
+if victoire == False:
+   if len(cartes1) < len(cartes2):
+      print(2)
+   elif len(cartes1) > len(cartes2):
+      print(1)
+   elif len(cartes1) == len(cartes2):
+      print("=")
+
+print(nbTours)
