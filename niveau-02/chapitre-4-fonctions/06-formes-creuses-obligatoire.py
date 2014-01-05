4b223f3485da5249e77a1856a421ace6b317a926
PiccandN <jagermoffin@gmail.com>
2014-01-05 14:53:45 +0100

    Update 06-formes-creuses-obligatoire.py

--- niveau-02/chapitre-4-fonctions/06-formes-creuses-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/06-formes-creuses-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 06-formes-creuses-obligatoire.py
 # nom de l'exercice : Formes creuses
@@ -17,3 +16,64 @@
 
 # mettre votre code ici
 
+nbX = int(input())
+nbLignesRectangle = int(input())
+nbColonnesRectangle = int(input())
+nbLignesTriangle = int(input())
+
+
+def ligneX(nbX):
+   for loop in range(nbX):
+      print("X", end = "")
+   print("")
+   
+
+def rectangle(nbLignesRectangle, nbColonnesRectangle):
+   if nbLignesRectangle == 1:
+      for loop in range(nbColonnesRectangle):
+         print("#", end = "")
+      print("")
+      
+   elif nbColonnesRectangle == 1:
+      for loop in range(nbLignesRectangle):
+         print("#")
+   
+   else:
+      for loop in range(nbColonnesRectangle):
+        print("#", end = "")
+      print("")   
+   
+      for loop in range(nbLignesRectangle - 2):
+         print("#", end = "")
+         for loop in range(nbColonnesRectangle - 2):
+            print(" ", end = "")
+         print("#")
+   
+      for loop in range(nbColonnesRectangle):
+         print("#", end = "")
+      print("")
+   
+
+def triangle(nbLignesTriangle):
+   if nbLignesTriangle == 1:
+      print("@")
+   
+   else:
+      print("@")
+   
+      for i in range(nbLignesTriangle - 2):
+         print("@", end = "")
+         for loop in range(i):
+            print(" ", end = "")
+         print("@", end = "")
+         print("")
+   
+      for loop in range(nbLignesTriangle):
+         print("@", end = "")
+   
+   
+ligneX(nbX)
+print("")
+rectangle(nbLignesRectangle, nbColonnesRectangle)
+print("")
+triangle(nbLignesTriangle)
